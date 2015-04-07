3c9379d0bee866f8181d7803b34d1bbf2d079da4
Jonathan Lin & Jill Kuchman <student@epicodus.com>
2015-04-07 09:56:40 -0700

    Add code to pass test every word in multiple word sentence

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 var titleCase = function(input) {
-    var caps_first_letter = input.charAt(0).toUpperCase();
-    var length = input.length;
-    var rest_of_word = input.substring(1,length);
-    var output = caps_first_letter + rest_of_word;
-    return output;
+
+    var split_Sentence = input.split(" ");
+    var return_array = [];
+    for (var i = 0; i < split_Sentence.length; i++)
+    {
+        console.log(split_Sentence[i]);
+        var caps_first_letter = split_Sentence[i].charAt(0).toUpperCase();
+        var length = split_Sentence[i].length;
+        var rest_of_word = split_Sentence[i].substring(1,length);
+        var output = caps_first_letter + rest_of_word;
+        return_array.push(output);
+    }
+    var return_string = return_array.join(' ');
+    return return_string;
 };
